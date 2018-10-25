752cdd069b1026b3112a7a88fbc9477c0e205dd7
Sascha K <kolsaa16@htlkaindorf.at>
2018-10-25 09:43:42 +0200

    origin

--- src/Inventory/Calculations.java
+++ src/Inventory/Calculations.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package BL;
+package Inventory;
 
 /**
  *
  * @author sasch
  */
-public class FrayteBL {
+public class Calculations {
     
 }
